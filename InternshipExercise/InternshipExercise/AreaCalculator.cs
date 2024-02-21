093883c11c4c1062b3dff2758cb5fce718399800
PowderDuck <veigarmainer14@gmail.com>
2024-02-21 16:40:56 +0000

    Internship Project

---
+++ InternshipExercise/InternshipExercise/AreaCalculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipExercise
+{
+    //Exercise 1;
+    public interface GeometricShape
+    {
+        public float GetArea(params float[] geometricValues);
+    }
+
+    public class Circle : GeometricShape
+    {
+        public float GetArea(params float[] geometricValues)
+        {
+            if (geometricValues.Length < 1f)
+                throw new Exception("Insufficient number of arguments, at least 1 needed !");
+
+            float radius = geometricValues[0];
+            return MathF.PI * radius * radius;
+        }
+    }
+
+    public class Triangle : GeometricShape
+    {
+        private readonly string[] triangleTypes = new string[] { "Common", "Isosceles", "Equilateral" };
+ 
+        public float GetArea(params float[] geometricValues)
+        {
+            if (geometricValues.Length < 3f)
+                throw new Exception("Insufficient number of arguments; at least 3 needed !");
+
+            int typeIndex = 1;
+            float a = geometricValues[0];
+            float b = geometricValues[1];
+            float c = geometricValues[2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                float currentSide = geometricValues[i];
+                typeIndex = 1;
+
+                for (int x = 0; x < 2; x++)
+                {
+                    typeIndex += currentSide == geometricValues[(i + x + 1) % 3] ? 1 : 0;
+                }
+
+                if(typeIndex > 1f)
+                {
+                    break;
+                }
+            }
+
+            float semiPerimeter = (a + b + c) / 2f;
+            float area = MathF.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+            Console.WriteLine($"{triangleTypes[typeIndex - 1]} Triangle;");
+
+            return area;
+        }
+    }
+
+    //Exercise 2;
+    /*
+     
+     CREATE TABLE Products(
+        ProductID INT PRIMARY KEY, 
+        ProductName VARCHAR(255) NOT NULL, 
+        Category VARCHAR(255)
+     );
+
+     SELECT ProductName, Category FROM Products;
+
+     */
+}
---
+++ InternshipExercise/InternshipExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using InternshipExercise;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GeometricShape circle = new Circle();
+        GeometricShape triangle = new Triangle();
+
+        Console.WriteLine($"Area of a Circle With the Radius of 5 is : {circle.GetArea(5)};\n");
+        Console.WriteLine($"Area of a Triangle With the Sides of 3, 4, 5 : {triangle.GetArea(3, 4, 5)};\n");
+        Console.WriteLine($"Area of a Triangle With the Sides of 5, 5, 6 : {triangle.GetArea(5, 5, 6)};\n");
+        Console.WriteLine($"Area of a Triangle With the Sides of 7, 7, 7 : {triangle.GetArea(7, 7, 7)};\n");
+    }
+}
